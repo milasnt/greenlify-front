c633ce4c77c2ff57bc52df6b47fd88869c5470c5
milasnt <camilaassnt@gmail.com>
2026-09-10 13:06:28 -0300

    fix: ajeitando estrutura de rotas para a forma recomendada

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './global.css'
-import { router } from './routes/Routes.tsx'
-import { RouterProvider } from 'react-router-dom'
+
+import FAQ from './routes/FAQ';
+import Contato from './routes/Contato';
+import Integrantes from './routes/Integrantes';
+import Sobre from './routes/Sobre';
+import Home from './routes/Home';
+import Error from './routes/Error';
+import App from './App';
+
+export const router = createBrowserRouter([
+  { path: "/", element: <App />, errorElement: <Error/>, children: [
+        { path: "", element: <Home /> },
+        { path: "sobre", element: <Sobre /> },
+        { path: "integrantes", element: <Integrantes /> },
+        { path: "contato", element: <Contato /> },
+        { path: "faq", element: <FAQ /> },  
+    ]
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
