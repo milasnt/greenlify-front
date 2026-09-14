0305ee05094d232e8fbf753bfb73610cde55049f
milasnt <camilassntt@gmail.com>
2026-09-13 21:37:16 -0300

    fix: implementando um scroll para o topo da página ao mudar de rota

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import { useEffect } from "react";
 import Header from "./components/Header";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Footer from "./components/Footer";
 
 export default function App() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <>
       <Header />
